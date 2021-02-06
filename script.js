141fde4a549cac7d70e3093d1e331ae29aabe5ae
Glitch (lemon-zingers) <none>
2021-02-06 08:14:18 +0000

    🐫🌖 Updated with Glitch

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}

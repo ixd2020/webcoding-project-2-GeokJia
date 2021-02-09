cf1f3d4c9bc97f49db91a0a201b6d23a86df01e4
Glitch (lemon-zingers) <none>
2021-02-09 16:06:13 +0000

    ⛺️📡 Updated with Glitch !!

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction2() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav2") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
